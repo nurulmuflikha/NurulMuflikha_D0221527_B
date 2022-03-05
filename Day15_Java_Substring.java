6f82eb2f12acfe8154d55c93188e9fa55948a0d0
nurulmuflikha <nurulmuflikhaalka@gmail.com>
2022-03-05 19:24:39 +0700

    day ngoding

---
+++ Day15_Java_Substring.java
@@ -0,0 +1,21 @@
+package yhummyChild;
+        
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+        
+public class Day15_Java_Substring {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String sub = S.substring(start, end);
+        System.out.println(sub);
+    }
+}
+    
+
